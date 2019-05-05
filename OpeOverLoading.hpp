27d7a7c07c89220eccce4ea811e3fb4000da92ee
YechengLi <31666353+YechengLi@users.noreply.github.com>
2019-05-05 12:55:51 -0400

    Update OpeOverLoading.hpp

--- OpeOverLoading.hpp
+++ OpeOverLoading.hpp
@@ -13,17 +13,10 @@
 #include <iostream>
 using namespace std;
 
-typedef vector<double> Vector;
-typedef vector<Vector> Matrix;
 
-Vector operator*(const Matrix& C,const Vector& V);
 vector<double> operator*(const double& a,const vector<double>& V);
-Vector operator+(const double& a,const Vector& V);
+vector<double> operator/(const vector<double>& V, const double& a);
 vector<double> operator+(const vector<double>& V,const vector<double>& W);
-Vector operator*(const Vector& V,const Vector& W);
-Vector exp(const Vector& V);
-double operator^(const Vector& V,const Vector& W);
-ostream & operator<<(ostream & out, Vector & V);
-ostream & operator<<(ostream & out, Matrix & W);
+
 
 #endif /* OpeOverLoading_hpp */
