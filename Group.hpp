0da35038aac0610ebe77972ea9e25679077317e7
YechengLi <31666353+YechengLi@users.noreply.github.com>
2019-05-05 12:57:10 -0400

    Update Group.hpp

--- Group.hpp
+++ Group.hpp
@@ -5,7 +5,6 @@
 //  Created by apple on 2019/5/2.
 //  Copyright © 2019年 Robot Wang. All rights reserved.
 //
-
 #ifndef Group_hpp
 #define Group_hpp
 
@@ -18,16 +17,16 @@ using namespace std;
 class Group{
 private:
     vector<string> tickerlist; //a vector containing all the ticker of the stocks in that Group
-    vector<double> ACAAR;
+    vector<double> AvgCAAR;
     map<string,Stock> MyStock;
     int num;
 public:
     vector<string> GetTickerlist(void){return tickerlist;}
-    vector<double> GetACAAR(void){return ACAAR;}
-    vector<double> CalACAAR();
+    vector<double> GetAvgCAAR(void){return AvgCAAR;}
+    void CalAvgCAAR();
     vector<double> CalCAAR();
     vector<double> AAR();
-    Group(vector<string> tickerlist_,vector<double> ACAAR_,map<string,Stock> MyStock_):tickerlist(tickerlist_),num(tickerlist.size()),MyStock(MyStock_){}
+    Group(vector<string> tickerlist_,map<string,Stock> MyStock_):tickerlist(tickerlist_),num(tickerlist.size()),MyStock(MyStock_){}
     Group(){}
     
     
