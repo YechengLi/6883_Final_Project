f44b146801a71591a5d440e3034f20a606af8de7
YechengLi <31666353+YechengLi@users.noreply.github.com>
2019-05-09 20:47:25 -0400

    Update RetrieveData.hpp

--- RetrieveData.hpp
+++ RetrieveData.hpp
@@ -7,9 +7,8 @@
 
 using namespace std;
 
-void ReadCsv(string filename, vector<string>& tickerList, 
-	map<string, Stock>& tickerMap, GroupType group);
+void ReadCsv(string filename, vector<string>& tickerList,
+             map<string, Stock>& tickerMap, GroupType group);
 int RetrieveFromYahoo(map<string, Stock>& tickerMap);
 int RetrieveSPY(map<string, double>& SPYPriceList);
 #endif
-
