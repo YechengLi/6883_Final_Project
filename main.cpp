1b6d135804ad48a5d13fe39f83c140eaab68dff9
YechengLi <31666353+YechengLi@users.noreply.github.com>
2019-05-09 20:48:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,12 @@
-//
-//  main.cpp
-//  Final Project
-//
-//  Created by apple on 2019/5/2.
-//  Copyright © 2019年 Robot Wang. All rights reserved.
-//
-
+#include <iostream>
 #include "OpeOverLoading.hpp"
 #include "Group.hpp"
+#include <map>
 #include "Stock.hpp"
 #include "RetrieveData.hpp"
-#include <iostream>
-#include <map>
 #include <vector>
 using namespace std;
+
 
 Group* GroupCalculation(vector<string>& TickerList, map<string, Stock>& Mystock, map<string, double>* SPYptr)
 {
@@ -24,119 +17,184 @@ Group* GroupCalculation(vector<string>& TickerList, map<string, Stock>& Mystock,
     GroupPtr->CalAvgCAAR();
     return GroupPtr;
 }
-
-//void plotResults(double xData[], vector<double> yData, vector<double> yData2, vector<double> yData3, int dataSize) {
-//	FILE *gnuplotPipe, *tempDataFile;
-//	const char *tempDataFileName;
-//	double x, y, x2, y2, x3, y3;
-//	int i;
-//	tempDataFileName = "tempData";
-//	gnuplotPipe = popen("/opt/local/bin/gnuplot", "w");
-//	if (gnuplotPipe) {
-//		fprintf(gnuplotPipe, "plot \"%s\" with lines\n", tempDataFileName);
-//		fflush(gnuplotPipe);
-//		tempDataFile = fopen(tempDataFileName, "w");
-//		for (i = 0; i < dataSize; i++) {
-//			x = xData[i];
-//			y = yData[i];
-//			cout << x << endl << y << endl;
-//			fprintf(tempDataFile, "%lf %lf\n", x, y);
-//		}
-//
-//		for (i = 0; i < dataSize; i++) {
-//			x2 = xData[i];
-//			y2 = yData2[i];
-//			cout << x2 << endl << y2 << endl;
-//			fprintf(tempDataFile, "%lf %lf\n", x2, y2);
-//		}
-//
-//		for (i = 0; i <= dataSize; i++) {
-//			x3 = xData[i];
-//			y3 = yData3[i];
-//			fprintf(tempDataFile, "%lf %lf\n", x3, y3);
-//		}
-//
-//		fclose(tempDataFile);
-//		printf("press enter to continue...");
-//		getchar();
-//		remove(tempDataFileName);
-//		fprintf(gnuplotPipe, "exit \n");
-//	}
-//	else {
-//		printf("gnuplot not found...");
-//	}
-//}
+void plotResults(double xData[], vector<double> yData, vector<double> yData2, vector<double> yData3, int dataSize) {
+    FILE *gnuplotPipe,*tempDataFile;
+    const char *tempDataFileName1 = "Group Beat";
+    const char *tempDataFileName2 = "Group Meet";
+    const char *tempDataFileName3 = "Group Miss";
+    double x,y, x2, y2,x3,y3;
+    int i;
+    gnuplotPipe = popen("/opt/local/bin/gnuplot","w");
+    if (gnuplotPipe) {
+        fprintf(gnuplotPipe,"plot \"%s\" with lines, \"%s\" with lines, \"%s\" with lines\n",tempDataFileName1, tempDataFileName2,tempDataFileName3);
+        fflush(gnuplotPipe);
+        tempDataFile = fopen(tempDataFileName1,"w");
+        for (i=0; i < dataSize; i++) {
+            x = xData[i];
+            y = yData[i];
+            fprintf(tempDataFile,"%lf %lf\n",x,y);
+        }
+        fclose(tempDataFile);
+        tempDataFile = fopen(tempDataFileName2,"w");
+        for (i=0; i < dataSize; i++) {
+            x2 = xData[i];
+            y2 = yData2[i];
+            fprintf(tempDataFile,"%lf %lf\n",x2,y2);
+        }
+        fclose(tempDataFile);
+        tempDataFile = fopen(tempDataFileName3,"w");
+        for (i=0; i < dataSize; i++) {
+            x3 = xData[i];
+            y3 = yData3[i];
+            fprintf(tempDataFile,"%lf %lf\n",x3,y3);
+        }
+        fclose(tempDataFile);
+        
+//        printf("press enter to continue...");
+        getchar();
+//        remove(tempDataFileName1);
+//        remove(tempDataFileName2);
+        fprintf(gnuplotPipe,"exit \n");
+    } else {
+        printf("gnuplot not found...");
+    }
+}
 
 int main()
 {
     vector<string> BeatList, MeetList, MissList;
     map<string, Stock> BeatMap, MeetMap, MissMap;
-    ReadCsv("beat.csv", BeatList, BeatMap, Beat);
-    ReadCsv("meet.csv", MeetList, MeetMap, Meet);
-    ReadCsv("miss.csv", MissList, MissMap, Miss);
-    RetrieveFromYahoo(BeatMap);
-    RetrieveFromYahoo(MeetMap);
-    RetrieveFromYahoo(MissMap);
     map<string, double> SPY;
-    RetrieveSPY(SPY);
-    map<string, double>* SPYptr = CalReturn(SPY);
-	//test Display()
-	BeatMap["AMZN"].Display();
+    map<string, double>* SPYptr = nullptr;
     
-    Group* BeatPtr = GroupCalculation(BeatList, BeatMap, SPYptr);
-    Group* MeetPtr = GroupCalculation(MeetList, MeetMap, SPYptr);
-    Group* MissPtr = GroupCalculation(MissList, MissMap, SPYptr);
+    Group* BeatPtr = nullptr;
+    Group* MeetPtr = nullptr;
+    Group* MissPtr = nullptr;
     
-    //for (map<string, Stock>::iterator itr = BeatMap.begin();itr!= BeatMap.end();itr++) {
-    //    itr->second.CalAR(SPYptr);
-    //}
-    //Group BeatGroup(BeatList, BeatMap);
-    //BeatGroup.CalAvgCAAR();
-    //
-    //for (map<string, Stock>::iterator itr = MeetMap.begin(); itr != MeetMap.end(); itr++) {
-    //    itr->second.CalAR(SPYptr);
-    //}
-    //Group MeetGroup(MeetList, MeetMap);
-    //MeetGroup.CalAvgCAAR();
-    
-    //for (map<string, Stock>::iterator itr = MissMap.begin(); itr != MissMap.end(); itr++) {
-    //    itr->second.CalAR(SPYptr);
-    //}
-    //Group MissGroup(MissList, MissMap);
-    //MissGroup.CalAvgCAAR();
-    
-    for (int i = 0; i <= 10; i++)
-    {
-        cout << BeatPtr->GetAvgCAAR()[i] << endl;
-    }
-    
-    for (int i = 0; i <= 10; i++)
-    {
-        cout << MeetPtr->GetAvgCAAR()[i] << endl;
-    }
-    
-    for (int i = 0; i <= 10; i++)
-    {
-        cout << MissPtr->GetAvgCAAR()[i] << endl;
-    }
+    Matrix DataMatirx;
+    VectorV BeatVector(2);
+    VectorV MeetVector(2);
+    VectorV MissVector(2);
 
-	//double Time[120];
-	//for (int i = 0; i < 120; i++) {
-	//	Time[i] = i + 1;
-	//}
-	//double* ptr = Time;
-	//vector<double> yData = BeatPtr->GetAvgCAAR();
-	//vector<double> yData2 = MeetPtr->GetAvgCAAR();
-	//vector<double> yData3 = MissPtr->GetAvgCAAR();
-	//plotResults(Time, yData, yData2, yData3, 120);
+    int Option;
+
+    do
+    {
+        cout << "Please enter the options' number to proceed:" << endl;
+        cout << "1. Retrieve historical price data for all stocks." << endl
+             << "2. Pull information for on stock from one group." << endl
+             << "3. Show AAR or CAAR for one group." << endl
+             << "4. Show the Gunplot graphs with CAAR for all 3 groups." << endl
+             << "5. Exit your program" << endl;
+        cout << endl;
+        cout << "Enter your option: "; cin >> Option;
+        
+        switch(Option)
+        {
+            case 1:{
+                ReadCsv("beat.csv", BeatList, BeatMap, Beat);
+                ReadCsv("meet.csv", MeetList, MeetMap, Meet);
+                ReadCsv("miss.csv", MissList, MissMap, Miss);
+                RetrieveFromYahoo(BeatMap);
+                RetrieveFromYahoo(MeetMap);
+                RetrieveFromYahoo(MissMap);
+                RetrieveSPY(SPY);
+                SPYptr = CalReturn(SPY);
+                cout << "Retrieve completed" << endl;
+                break;
+            }
+                
+            case 2:{
+                char temp;
+                do{
+                    string TickerName;
+                    cout << "Please enter the stock's ticker: "; cin >> TickerName;
+                    int x =0;
+                    for (int i=0; i < BeatList.size(); i++)
+                    { if(BeatList[i] == TickerName) {BeatMap[TickerName].Display(); x=1;}}
+                    for (int i=0; i < MeetList.size(); i++)
+                    { if(MeetList[i] == TickerName) {MeetMap[TickerName].Display(); x=1;}}
+                    for (int i=0; i < MissList.size(); i++)
+                    { if(MissList[i] == TickerName) {MissMap[TickerName].Display(); x=1;}}
+                    if (x == 0) cout << "Your Ticker is not in S&P500" << endl;
+                    cout << endl;
+                    cout << "Do you want to look for other stock's information? (Y/N) "; cin >> temp;
+                }
+                while (temp == 'Y' || temp == 'y');
+                    break;
+            }
+            
+            case 3:{
+                
+                BeatPtr = GroupCalculation(BeatList, BeatMap, SPYptr);
+                MeetPtr = GroupCalculation(MeetList, MeetMap, SPYptr);
+                MissPtr = GroupCalculation(MissList, MissMap, SPYptr);
+                
+                BeatVector[0] = BeatPtr->GetAvgAAR();
+                BeatVector[1] = BeatPtr->GetAvgCAAR();
+                MeetVector[0] = MeetPtr->GetAvgAAR();
+                MeetVector[1] = MeetPtr->GetAvgCAAR();
+                MissVector[0] = MissPtr->GetAvgAAR();
+                MissVector[1] = MissPtr->GetAvgCAAR();
+                DataMatirx.push_back(BeatVector);
+                DataMatirx.push_back(MeetVector);
+                DataMatirx.push_back(MissVector);
+                
+                int GroupName;
+                int DataName;
+                string GroupString[3] = { "Beat", "Meet", "Miss"};
+                string DataString[2] = {"AAR", "CAAR"};
+                
+                char temp;
+                do{
+                    cout << "1.Beat   2.Meet  3.Miss:  "; cin >> GroupName;
+                    cout << "1.AAR   2.CAAR:  "; cin >> DataName;
+                    cout << GroupString[GroupName-1] << " " << DataString[DataName-1] << endl;
+                    cout << DataMatirx[GroupName-1][DataName-1] << endl;
+                    cout << "Do you want to look for other data? (Y/N)  "; cin >> temp;
+                    cout << endl;
+                }
+                while (temp == 'Y' || temp == 'y');
+                    break;
+                
+            }
+            case 4:{
+            
+                BeatPtr = GroupCalculation(BeatList, BeatMap, SPYptr);
+                MeetPtr = GroupCalculation(MeetList, MeetMap, SPYptr);
+                MissPtr = GroupCalculation(MissList, MissMap, SPYptr);
+                
+                double Time[120];
+                for(int i=0;i<120;i++){
+                    Time[i] = i-59;
+                }
+                
+                vector<double> yData = BeatPtr->GetAvgCAAR();
+                vector<double> yData2 = MeetPtr->GetAvgCAAR();
+                vector<double> yData3 = MissPtr->GetAvgCAAR();
+                
+                plotResults(Time,yData, yData2, yData3, 120);
+                cout << "Graph drawn" << endl
+                     << endl;
+                break;
+                
+            }
+                
+            
+            case 5:{
+                cout << "Promgram terminated!" << endl
+                     << endl
+                     << endl
+                     << endl
+                     << endl
+                     << "Good luck with your finals and have a good summer vacation!!!!!!!!" << endl
+                     << endl
+                     << endl;
+                break;
+            }
+        }
+    }
+    while(Option >0 && Option <5);
     
-    
-    
-    
-    delete SPYptr;
-    delete BeatPtr;
-    delete MeetPtr;
-    delete MissPtr;
-    
-    return 0;
+return 0;
 }
