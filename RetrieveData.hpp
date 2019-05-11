9d982a9b1541db2861ba28c546345678e2512480
dele20 <47578303+dele20@users.noreply.github.com>
2019-05-11 11:41:42 -0400

    Update RetrieveData.hpp

--- RetrieveData.hpp
+++ RetrieveData.hpp
@@ -10,7 +10,7 @@ using namespace std;
 // load EPS-related data from csv and store them in Stock objects
 void ReadCsv(string filename, vector<string>& tickerList,
              map<string, Stock>& tickerMap, GroupType group);
-// download price data for one group of stocksfrom Yahoo Finance and store them
+// download price data for one group of stocks from Yahoo Finance and store them
 int RetrieveFromYahoo(map<string, Stock>& tickerMap);
 // download prices for SPY during 2017-2018
 int RetrieveSPY(map<string, double>& SPYPriceList);
